9693da604da8893d8b2af986a7b3372a76fa5e6d
Marián <118210286+KufaM@users.noreply.github.com>
2024-06-20 08:00:04 +0200

    Update server.js - poznámky

--- chess-main/server.js
+++ chess-main/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const {
-  createServer
-} = require('node:http');
-const {
-  join
-} = require('node:path');
-const {
-  Server
-} = require('socket.io');
-
+// Importuje funkci pro vytvoření HTTP serveru z Node.js
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+// Importuje knihovnu socket.io pro real-time komunikaci
+const { Server } = require('socket.io');
+// Vytvoří instanci Express aplikace
 const app = express();
+// Vytvoří HTTP server pomocí Express aplikace
 const server = createServer(app);
+
+// Vytvoří instanci Socket.IO serveru připojenou k HTTP serveru
 const io = new Server(server);
 const port = 3000;
 
+// Nastaví statickou složku pro Express aplikaci
 app.use(express.static(join(__dirname, 'static')))
 
 // URL adresy
@@ -27,26 +27,30 @@ app.get('/:roomId', (req, res) => {
 
 let rooms = {};
 
+//připojení nového uživatele
 io.on('connection', (socket) => {
   console.log(`${socket.id} user connected`);
+  
+   // Nastaví událost pro odpojení uživatele
   socket.on('disconnect', () => {
     console.log(`${socket.id} user disconnected`);
   });
+   // vytvoření nové místnosti
   socket.on('createRoom', (response) => {
-    const roomId = generateRoomId();
+    const roomId = generateRoomId();  // Vygeneruje unikátní ID pro místnost
     rooms[roomId] = {
-      players: [socket.id],
+      players: [socket.id],     // Přidá aktuálního uživatele jako prvního hráče
       playerNames: {
-        white: socket.id,
+        white: socket.id,    
         black: 'Waiting...'
       }
     };
-    socket.join(roomId);
-    response({
+    socket.join(roomId);         // Připojí uživatele k místnosti
+    response({        // Vrátí ID místnosti klientovi
       roomId
     });
   });
-
+  // Nastaví událost pro připojení k existující místnosti
   socket.on('joinRoom', (roomId, response) => {
     if (rooms[roomId] && rooms[roomId].players.length < 2) {
       rooms[roomId].players.push(socket.id);
@@ -62,6 +66,8 @@ io.on('connection', (socket) => {
       });
     }
   });
+
+  / Nastaví událost pro získání jmen hráčů
   socket.on('player_names', (roomId, playerId, response) => {
     console.log('player names triggered');
     io.to(roomId).emit('new_player_names', {
@@ -79,6 +85,7 @@ server.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
 });
 
+// Funkce co generuje unikátní ID místnosti
 function generateRoomId() {
   return Math.random().toString(36).substr(2, 9);
 }
